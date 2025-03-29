24867d0bcce53f70d787616f8c92398667f0cebf
Andrew Park <andrew@aycecreative.com>
2025-03-29 08:31:05 -0600

    Update project files

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,19 @@ import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     hydrogen(),
     oxygen(),
     remix({
-      presets: [hydrogen.preset()],
+      presets: [hydrogen.v3preset()],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_lazyRouteDiscovery: true,
+        v3_routeConfig: true,
         v3_singleFetch: true,
       },
     }),
